f0d42643036cf86ff7e46c181e93727c516d49ba
jeferssonnunez <jeferssonnunez@gmail>
2013-12-04 17:57:58 -0500

    clase operacion modificada

--- calculadora/src/clase/Operacion.java
+++ calculadora/src/clase/Operacion.java
@@ -2,8 +2,26 @@ package clase;
 
 public class Operacion {
 
-	public Operacion() {
-		// TODO Auto-generated constructor stub
-	}
+public double sumar(double n1, double n2){
+	    
+        double suma = n1 + n2;
+        return suma;
+    }
+    
+    public double restar(double n1, double n2){
+    
+        double resta = n1 - n2;
+        return resta;
+    }
+    
+    public double multiplicar (double n1, double n2){
+        double producto = n1 * n2;
+        return producto;
+    }
+    
+    public double dividir (double n1, double n2){
+        double division = n1 / n2;
+        return division;
+    }
 
 }
