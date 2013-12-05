d6f36dceebfaca0752f96adc5be357ed768054e2
jeferssonnunez <jeferssonnunez@gmail>
2013-12-04 23:11:18 -0500

    resta en Operacion.java creando rama1

--- calculadora/src/clase/Operacion.java
+++ calculadora/src/clase/Operacion.java
@@ -7,6 +7,11 @@ public double sumar(double n1, double n2){
         double suma = n1 + n2;
         return suma;
     }
+
+public double restar(double n1, double n2){
     
+    double resta = n1 - n2;
+    return resta;
+}    
 
 }
